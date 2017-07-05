429a09ccc389c24b2768660dfc9fb3e3c9544a18
Saptadeep Debnath <saptadeep.deb@gmail.com>
2017-07-05 20:12:59 +0400

    Update distance_cal.cpp

--- distance_cal.cpp
+++ distance_cal.cpp
@@ -10,21 +10,20 @@ int main()
     cin>>yP;
     cout<<"Enter value for xP: ";
     cin>>xP;
-    int h = 238;
-    double alpha = (30 * M_PI) / 180;
-    double theta_v = (38.795 * M_PI) / 180;
+    int h = 238; //height of the quad
+    double alpha = (30 * M_PI) / 180; //angle of inclination
+    double theta_v = (34.046 * M_PI) / 180; //vertical FOV
     
     double theta2 = (theta_v - alpha + acos( (cos(theta_v) ) * cos(alpha)))/2;
     double z = (2 * h) / (1 / tan(theta2)) + (1 / tan(alpha));
-    double x = h / tan(alpha + theta2);
-    double T = (z * (640 - yP)) / 640;
-    double t = T + x;
-    cout<<t<<endl;
+    double r = h / tan(alpha + theta2);
+    double T = (z * (480 - yP)) / 480;
+    double y = T + r; //local y distance in real life
+    cout<<y<<endl;
     
-    double U = ((fabs(240 - xP)) * z) / 640;
-    if(240 - xP > 0)
+    if(320 - xP > 0)
         cout<<"Move left\n";
-    else if(240 - xP < 0)
+    else if(320 - xP < 0)
         cout<<"Move right\n";
     return 0;
     
